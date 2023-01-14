2868589f1a75d8cbaef40371465ed2cfe0765a3d
KevinDGnanih <kevin.gnanih@gmail.com>
2023-01-14 23:17:12 +0000

    updated the skills section for a better UI/UX exp

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Tooltip as ReactTooltip } from 'react-tooltip';
+import { Tooltip } from 'react-tooltip'
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
@@ -71,14 +71,14 @@ const Skills = () => {
                     <h4 className='bold-text'>{work.name}</h4>
                     <p className='p-text'>{work.company}</p>
                   </motion.div>
-                  <ReactTooltip
+                  <Tooltip
                     id={work.name}
                     effect='solid'
                     arrowColor='#fff'
                     className='skills-tooltip'
                   >
                     {work.desc}
-                  </ReactTooltip>
+                  </Tooltip>
                 </>
               ))}
             </motion.div>
@@ -90,4 +90,8 @@ const Skills = () => {
   );
 };
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'),
+  'skills',
+  'app__whitebg',
+);
